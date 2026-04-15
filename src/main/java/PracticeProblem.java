ad901b2ff6f26a559496830389f31d2668c3ba17
Nithin Alagar <343954178@tdsb.ca>
2026-04-15 10:45:15 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,22 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean isPrime(int num){
+	for (int i = 0; i < num; i++){
+		if (i%2==0){
+			return false;
+		}
+	}
+	return true;
+	
+	}
+	public static String hasCapital(String word){
+	for (int i = 0; i < word.length() - 1; i++ ){
+		if (word.substring(i, i + 1).toUpperCase() == word.substring(i, i + 1)){
+			return true;
+		}
+	}
+	return false;
+}
 
 }
