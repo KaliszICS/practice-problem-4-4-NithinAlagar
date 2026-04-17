8e2da73a4aee3df61b4d421da27ec28636b0d590
Nithin Alagar <343954178@tdsb.ca>
2026-04-17 10:58:59 -0400

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -17,7 +17,7 @@ public class PracticeProblem {
 	}
 	public static boolean hasCapital(String word){
 	for (int i = 0; i < word.length() - 1; i++ ){
-		if (word.substring(i, i + 1).toUpperCase() == word.substring(i, i + 1)){
+		if (word.substring(i, i).toUpperCase() == word.substring(i, i)){
 			return true;
 		}
 	}
